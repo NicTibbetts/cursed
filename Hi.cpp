0d386ab0fcdd36fc663a9292e732beb8e6fde99b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤNic Tibbetts <48816030+NicTibbetts@users.noreply.github.com>
2026-08-21 16:49:14 -0700

    Update Hi.cpp

--- Hi.cpp
+++ Hi.cpp
@@ -1,7 +1,7 @@
 /************************************************************
  *  Author:         Nicholas Tibbetts
- *  Date:           03/11/2024 T12:55:02
- *  License:        Copyright 2024 Nic Tibbetts
+ *  Date:           08/21/2026 T04:20:43
+ *  License:        Copyright 2026 Nic Tibbetts
  *  Description:    Cursed Coding
  ***********************************************************/
 
@@ -9,32 +9,31 @@
 using namespace std;
 
 int main()
-{
-    unsigned long long __[] = {
-        ((0xAULL << 56) | (0x27ULL << 48) | (0x2EULL << 40) |
-         (0x2EULL << 32) |
-         (0x2DULL << 24) | (0x6EULL << 16) | (0x62ULL << 8) |
-         0x15ULL) ^
-            (~(~0ULL)),
-        (~0ULL -
-         ((0xD2ULL << 56) | (0xCFULL << 48) | (0xD1ULL << 40) |
-          (0xD9ULL << 32) | (0x9CULL << 24) | (0x21ULL << 16) |
-          (0xFFULL << 8) | 0x21ULL)),
-        ((0x4EULL << 56) | (0x69ULL << 48) | (0x63ULL << 40) |
-         (0x54ULL << 32) | (0x69ULL << 24) | (0x62ULL << 16) |
-         (0x62ULL << 8) | 0x73ULL) // ~:)
-    };
-    for (auto _ = -0x0; _ < static_cast<int>(((+~+-+-~__[1]) >> 63) +
-                                             ((__[0] != 0) << 3) + ((__[1] != 0) << 2) - ~0);
-         ++_)
+{ unsigned long long __[] = {
+        ((0xACULL << 56) | (0xBEULL << 48) | (0x1AULL << 40) | (0xEBULL << 32) |
+         (0x3AULL << 24) | (0xF9ULL << 16) | (0x91ULL << 8) | 0xEEULL),
+        ((0x4EULL << 56) | (0x69ULL << 48) | (0x63ULL << 40) | (0x60ULL << 32) |
+         (0x81ULL << 24) | (0x85ULL << 16) | (0x8FULL << 8) | 0x91ULL),
+        ((0x4EULL << 56) | (0x69ULL << 48) | (0x63ULL << 40) | (0x54ULL << 32) |
+         (0x69ULL << 24) | (0x62ULL << 16) | (0x62ULL << 8) | 0x73ULL) // ~:)
+    }, ___ = (2[__] ^ (2[__] >> 11)) *
+             ((2[__] ^ (2[__] >> 23) ^ (2[__] << 17)) | -~0ULL);
+    for (auto _ = ~+~0; _ < static_cast<int>(((2[__] & 0xFULL) << 2) | !0); ++_)
     { //!
-        { auto ____ = [&__](auto _, auto $)
-            { return static_cast<unsigned char>(
-                    (($ < (1 << 3) ? -+-+__[0] >> (((1 << 6) ^ -+(1 << 3)) - ($) * (1 << 3))
-                                   : ~+-+-~__[1] >> (56 + -+($ ^ +-8) * 8)) &
-                     ((1 << 8) - 1)) ^
-                    -+-(((0x20 << 1) ^ +0x2 ^ -~+~-(__[(0x7 ^ +0x5)] >> 63)) & 0x7F) ^ (~-~+~(~$ & 0x1) & 0x0)); 
-            }; 
-            { cout << ____(__, _); } } } cout << (char)((1 << 3) | (1 << 1)); 
-        }
-
+        auto ____ = [&__, &___](auto _, auto $)
+        { auto _____ = static_cast<unsigned char>(
+                (((($ >> 3)[__] ^ 2[__]) >> (($ & 7) << 3)) & ((1ULL << 8) + ~0ULL)) ^
+                (((___ ^ ((2[__] >> ((_ & 7) << 3)) & ((1ULL << 8) + ~0ULL)) ^
+                   (static_cast<unsigned long long>(_ + $) * 2[__])) *
+                  ((2[__] ^ (2[__] >> 23) ^ (2[__] << 17)) |-~0ULL)) >> 56));
+            auto $_ = (_____ + ((2[__] >> ((_ & 7) << 3)) & 0xFFULL) + $) & 63;
+            return ___ ^= (static_cast<unsigned long long>(_____ + !0) *
+                           ((2[__] ^ (2[__] >> 29) ^ (2[__] << 7)) |
+                          -~0ULL)) ^ (static_cast<unsigned long long>(_) 
+                          * 2[__]) ^ $, ___ = ((___ << $_) | (___ >> ((-$_) & 63))) 
+                          ^ 2[__], _____; }; 
+                         cout << ____(_, (_ * ((2[__] >> 36) & 0xFULL) +
+                         ((___ >> 28) & 0x1FULL) * ((2[__] >> 24) & 0xFULL) +
+                         (2[__] >> 8 & 0xFULL)) % (((2[__] & 0xFULL) << 2) | !0));
+    } cout << (char)((___ >> 60)^((2[__] >> 36) & 0xFULL));
+}
